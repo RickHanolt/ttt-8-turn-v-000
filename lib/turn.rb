c433912babea34df58f8cf6fdcdfdd38ae88b6b2
Richard Hanolt <rhanolt@gmail.com>
2020-04-11 02:34:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,5 +18,8 @@ def valid_move?(board, index)
   if (board[index] == "" || board[index] == " " || board[index] == "  " || board[index] == "   " || board[index] == nil) && index.between?(0,8)
     true
   elsif board[index] == "X" || board[index] == "O"
+    false
+  else
+    false
   end
 end
